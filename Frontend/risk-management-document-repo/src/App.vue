4c024575fe6261ca69032ed910824ef7297b0ca8
Declan Rhodes <declanrhodesss@gmail.com>
2021-11-05 11:28:31 +0000

    feat: added sign out button

--- Frontend/risk-management-document-repo/src/App.vue
+++ Frontend/risk-management-document-repo/src/App.vue
@@ -3,12 +3,35 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/documents">Documents</router-link>
+      <router-link to="/documents">Documents</router-link> |
+      <router-link to="/accounts">Accounts</router-link>
+      <b-button style="float: right" @click="signOut">Sign Out</b-button>
     </div>
     <router-view/>
   </div>
   <!--TODO: Add logout button-->
 </template>
+
+<script>
+import Vue from 'vue'
+import VueCookies from 'vue-cookies'
+
+Vue.use(VueCookies)
+
+export default {
+  methods:{
+    signOut(){
+      if(this.$cookies.get('user')){
+          this.$cookies.remove('user')
+          this.$router.push('/')
+        }
+        else{
+            this.$router.push('unauthorized')
+        }
+    }
+  }
+}
+</script>
 
 <style>
 #app {
